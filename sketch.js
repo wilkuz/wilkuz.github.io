fb1e98355c18f6df33c5682864dc4894dd5b201b
wilkuz <zhupercycle@protonmail.com>
2022-02-22 13:07:53 +0100

    activate sketch

--- sketch.js
+++ sketch.js
@@ -16,25 +16,25 @@ function setup() {
    stroke(255);
  }
 
-// function draw() {
-//   let noiseScale = 0.01;
-//   	background("rgba(255,248,232,0.3)");
-//   for(let i=0; i < num; i++){
-//     let p = sketchParticles[i];
-//     let c = color(171,135,255);
-//     fill(c);
-//     stroke(c);
-//     ellipse(p.x, p.y, 1, 1);
-//     let n = noise(p.x * noiseScale, p.y * noiseScale);
-//     let a = TAU * n;
-//     p.x += cos(a)*2;
-//     p.y += sin(a)*3;
-//     if(!onScreen(p)) {
-//       p.x = random(width);
-//       p.y = random(height);
-//     }
-//   }
-// }
+function draw() {
+  let noiseScale = 0.01;
+  	background("rgba(255,248,232,0.3)");
+  for(let i=0; i < num; i++){
+    let p = sketchParticles[i];
+    let c = color(171,135,255);
+    fill(c);
+    stroke(c);
+    ellipse(p.x, p.y, 1, 1);
+    let n = noise(p.x * noiseScale, p.y * noiseScale);
+    let a = TAU * n;
+    p.x += cos(a)*2;
+    p.y += sin(a)*3;
+    if(!onScreen(p)) {
+      p.x = random(width);
+      p.y = random(height);
+    }
+  }
+}
 
  function mouseReleased(scale) {
    noiseSeed(millis());
